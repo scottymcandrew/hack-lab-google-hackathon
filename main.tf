4d9b52f106c8d4e554d139d729eea14c297463bf
Scott McAndrew <scottymcandrew@outlook.com>
2019-09-25 17:02:17 +0100

    Added service account to vm-series to access bootstrap bucket

--- main.tf
+++ main.tf
@@ -148,6 +148,9 @@ resource "google_compute_instance" "vm-series" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring.write",
     ]
+    email = [
+      "vm-series@scott-client-test.iam.gserviceaccount.com"
+    ]
   }
   network_interface {
     subnetwork = "mgmt-net-${var.subnetOctet}"
